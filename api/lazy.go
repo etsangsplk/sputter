bb7e0a516d02194f8612a875d0190b8367a0505c
Thomas Bradford <thom@kode4food.it>
2017-03-19 15:27:38 +0100

    lazy mapper and filter

--- api/lazy.go
+++ api/lazy.go
@@ -3,34 +3,37 @@ package api
 // ValueMapper returns a mapped representation of the specified Value
 type ValueMapper func(Value) Value
 
-// lazySequence will only resolve its first and rest when requested
-type lazySequence struct {
+// ValueFilter returns true if the Value remains part of a filtered Sequence
+type ValueFilter func(Value) bool
+
+// lazyMapper will only resolve its first and rest when requested
+type lazyMapper struct {
 	sequence Sequence
 	mapper   ValueMapper
 	resolved bool
 	isSeq    bool
 	first    Value
-	rest     *lazySequence
+	rest     *lazyMapper
 }
 
-// NewLazySequence creates a new LazySequence that wraps the original
-// and only maps its values on demand
-func NewLazySequence(s Sequence, m ValueMapper) Sequence {
-	return &lazySequence{
+// NewLazyMapper creates a new lazy mapping Sequence that wraps the
+// original and only maps its values on demand
+func NewLazyMapper(s Sequence, m ValueMapper) Sequence {
+	return &lazyMapper{
 		sequence: s,
 		mapper:   m,
 	}
 }
 
-func (l *lazySequence) resolve() *lazySequence {
+func (l *lazyMapper) resolve() *lazyMapper {
 	if l.resolved {
 		return l
 	}
 
-	l.isSeq = l.sequence.IsSequence()
-	if l.isSeq {
+	if l.sequence.IsSequence() {
+		l.isSeq = true
 		l.first = l.mapper(l.sequence.First())
-		l.rest = &lazySequence{
+		l.rest = &lazyMapper{
 			sequence: l.sequence.Rest(),
 			mapper:   l.mapper,
 		}
@@ -41,24 +44,91 @@ func (l *lazySequence) resolve() *lazySequence {
 	return l
 }
 
-// First returns the first mapped Value from the LazySequence
-func (l *lazySequence) First() Value {
+// First returns the first mapped Value from the lazyMapper
+func (l *lazyMapper) First() Value {
 	return l.resolve().first
 }
 
-// Rest returns the rest of the LazySequence
-func (l *lazySequence) Rest() Sequence {
+// Rest returns the rest of the lazyMapper
+func (l *lazyMapper) Rest() Sequence {
 	return l.resolve().rest
 }
 
 // IsSequence returns whether this instance is a consumable Sequence
-func (l *lazySequence) IsSequence() bool {
+func (l *lazyMapper) IsSequence() bool {
 	return l.resolve().isSeq
 }
 
 // Prepend creates a new Sequence by prepending a Value (won't be mapped)
-func (l *lazySequence) Prepend(v Value) Sequence {
-	return &lazySequence{
+func (l *lazyMapper) Prepend(v Value) Sequence {
+	return &lazyMapper{
+		first:    v,
+		rest:     l,
+		resolved: true,
+		isSeq:    true,
+	}
+}
+
+// lazyFilter will only resolve its first and rest when requested
+type lazyFilter struct {
+	sequence Sequence
+	filter   ValueFilter
+	resolved bool
+	isSeq    bool
+	first    Value
+	rest     *lazyFilter
+}
+
+// NewLazyFilter creates a new lazy filter Sequence that wraps the
+// original and only filters the next Value(s) on demand
+func NewLazyFilter(s Sequence, f ValueFilter) Sequence {
+	return &lazyFilter{
+		sequence: s,
+		filter:   f,
+	}
+}
+
+func (l *lazyFilter) resolve() *lazyFilter {
+	if l.resolved {
+		return l
+	}
+
+	for s := l.sequence; s.IsSequence(); s = s.Rest() {
+		if v := s.First(); l.filter(v) {
+			l.isSeq = true
+			l.first = v
+			l.rest = &lazyFilter{
+				sequence: s.Rest(),
+				filter:   l.filter,
+			}
+			break
+		}
+	}
+
+	l.resolved = true
+	l.sequence = nil
+	l.filter = nil
+	return l
+}
+
+// First returns the first mapped Value from the lazyFilter
+func (l *lazyFilter) First() Value {
+	return l.resolve().first
+}
+
+// Rest returns the rest of the lazyFilter
+func (l *lazyFilter) Rest() Sequence {
+	return l.resolve().rest
+}
+
+// IsSequence returns whether this instance is a consumable Sequence
+func (l *lazyFilter) IsSequence() bool {
+	return l.resolve().isSeq
+}
+
+// Prepend creates a new Sequence by prepending a Value (won't be mapped)
+func (l *lazyFilter) Prepend(v Value) Sequence {
+	return &lazyFilter{
 		first:    v,
 		rest:     l,
 		resolved: true,
--- api/lazy_test.go
+++ api/lazy_test.go
@@ -7,12 +7,12 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestLazyList(t *testing.T) {
+func TestLazyMapper(t *testing.T) {
 	as := assert.New(t)
 
 	c := 0
 	l := a.NewList("last").Prepend("middle").Prepend("first")
-	w := a.NewLazySequence(l, func(v a.Value) a.Value {
+	w := a.NewLazyMapper(l, func(v a.Value) a.Value {
 		c++
 		return "this is the " + v.(string)
 	})
@@ -40,4 +40,67 @@ func TestLazyList(t *testing.T) {
 	as.Equal(3, c, "prepend doesn't trigger mapper")
 	as.Equal("not mapped", v4, "prepended element retrieved")
 	as.Equal(w, r2, "prepended rest is the original sequence")
+}
+
+func TestLazyFilter(t *testing.T) {
+	as := assert.New(t)
+
+	c := 0
+	l := a.NewList("last").Prepend("filtered out").Prepend("first")
+	w := a.NewLazyFilter(l, func(v a.Value) bool {
+		c++
+		return v.(string) != "filtered out"
+	})
+
+	as.Equal(0, c, "nothing has been processed")
+
+	v1 := w.First()
+	as.Equal(1, c, "first element has been processed")
+	as.Equal("first", v1, "first element passed")
+
+	v2 := w.Rest().First()
+	as.Equal(3, c, "second element has been skipped")
+	as.Equal("last", v2, "third element passed")
+
+	r1 := w.Rest().Rest()
+	as.False(r1.IsSequence(), "finished")
+
+	p := w.Prepend("filtered out")
+	v4 := p.First()
+	r2 := p.Rest()
+	as.Equal(3, c, "prepend doesn't trigger filter")
+	as.Equal("filtered out", v4, "prepended element retrieved")
+	as.Equal(w, r2, "prepended rest is the original sequence")
+}
+
+func TestLazyFilteredAndMapped(t *testing.T) {
+	as := assert.New(t)
+
+	c1 := 0
+	c2 := 0
+	l := a.NewList("last").Prepend("middle").Prepend("first")
+	w1 := a.NewLazyFilter(l, func(v a.Value) bool {
+		c1++
+		return v.(string) != "middle"
+	})
+	w2 := a.NewLazyMapper(w1, func(v a.Value) a.Value {
+		c2++
+		return "this is the " + v.(string)
+	})
+
+	as.Equal(0, c1, "nothing has been processed")
+	as.Equal(0, c2, "nothing has been processed")
+
+	v1 := w2.First()
+	as.Equal(1, c1, "first element has been processed")
+	as.Equal(1, c2, "first element has been processed")
+	as.Equal("this is the first", v1, "first element mapped")
+
+	v2 := w2.Rest().First()
+	as.Equal(3, c1, "second element has been skipped")
+	as.Equal(2, c2, "second element is first mapper sees")
+	as.Equal("this is the last", v2, "last element mapped")
+
+	r1 := w2.Rest().Rest()
+	as.False(r1.IsSequence(), "finished")
 }
