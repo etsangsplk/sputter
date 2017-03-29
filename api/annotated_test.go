3c696f930f0e1ab0d46d48b9b8198df61e1d362e
Thomas Bradford <thom@kode4food.it>
2017-03-29 09:14:24 +0200

    some cleanup

--- api/annotated_test.go
+++ api/annotated_test.go
@@ -5,6 +5,7 @@ import (
 
 	a "github.com/kode4food/sputter/api"
 	"github.com/stretchr/testify/assert"
+	"strings"
 )
 
 func TestMetadata(t *testing.T) {
@@ -14,10 +15,14 @@ func TestMetadata(t *testing.T) {
 	v2 := v1.Merge(a.Metadata{"foo": false, "hello": "there"})
 
 	s1 := v1.String()
-	as.Equal(`{"foo" true, "bar" false}`, s1)
+	as.True(strings.Contains(s1, `"foo" true`))
+	as.True(strings.Contains(s1, `"bar" false`))
 
 	s2 := v2.String()
-	as.Equal(`{"bar" false, "hello" "there", "foo" false}`, s2)
+	as.True(strings.Contains(s2, `"bar" false`))
+	as.True(strings.Contains(s2, `"hello" "there"`))
+	as.True(strings.Contains(s2, `"foo" false`))
+	as.False(strings.Contains(s2, `"foo" true`))
 }
 
 func TestAnnotated(t *testing.T) {
--- api/arraymap.go
+++ api/arraymap.go
@@ -10,49 +10,10 @@ const (
 	ExpectedMapped = "expected a mapped value"
 )
 
-var keywords = make(Variables, 4096)
-
-// Keyword is an Atom-like Value that represents a Name for mapping purposes
-type Keyword struct {
-	name Name
-}
-
 // Mapped interfaces allow a Sequence item to be retrieved by Name
 type Mapped interface {
 	Sequence
 	Get(key Value) Value
-}
-
-// NewKeyword returns an interned instance of a Keyword
-func NewKeyword(n Name) *Keyword {
-	if r, ok := keywords[n]; ok {
-		return r.(*Keyword)
-	}
-	r := &Keyword{name: n}
-	keywords[n] = r
-	return r
-}
-
-func (k *Keyword) Name() Name {
-	return k.name
-}
-
-// Eval makes Keyword Evaluable
-func (k *Keyword) Eval(c Context) Value {
-	return k
-}
-
-// Apply makes Keyword Applicable
-func (k *Keyword) Apply(c Context, args Sequence) Value {
-	AssertArity(args, 1)
-	if m, ok := Eval(c, args.First()).(Mapped); ok {
-		return m.Get(k)
-	}
-	panic(ExpectedMapped)
-}
-
-func (k *Keyword) String() string {
-	return ":" + string(k.name)
 }
 
 // ArrayMap is a Mappable that is implemented atop an array
