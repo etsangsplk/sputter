a381930b125eac6ef9016258c3b62e264d79411a
Thomas Bradford <thom@kode4food.it>
2017-04-01 17:15:51 +0200

    channel and go coverage

--- builtins/concurrency_test.go
+++ builtins/concurrency_test.go
@@ -6,16 +6,32 @@ import (
 	a "github.com/kode4food/sputter/api"
 )
 
+func TestChannel(t *testing.T) {
+	ns := a.GetNamespace(a.UserDomain)
+	ns.Delete("c")
+	ns.Delete("e")
+	ns.Delete("s")
+	
+	testCode(t, `
+		(def c (channel))
+		(def e (:emit c))
+		(def s (:seq c))
+		
+		(go (e "hello"))	
+		(first s)
+	`, "hello")
+}
+
 func TestGoConcurrency(t *testing.T) {
 	ns := a.GetNamespace(a.UserDomain)
-	ns.Delete("x")
+	ns.Delete("g")
 	ns.Delete("r")
 
 	testCode(t, `
-		(def x (go
+		(def g (go
 			(emit 99)
 			(emit 100 1000)))
-		(def r (to-vector x))
+		(def r (to-vector g))
 		(+ (first r) (first (rest r)) (first (rest (rest r))))
 	`, a.NewFloat(1199))
 }
