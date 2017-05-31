fb879625eb2d32060465704a18b35bf3cf8f6d6e
Thomas Bradford <thom@kode4food.it>
2017-05-31 23:15:33 +0200

    starting work in closures

---
+++ evaluator/closure_test.go
@@ -0,0 +1,32 @@
+package evaluator_test
+
+import (
+	"testing"
+
+	a "github.com/kode4food/sputter/api"
+	"github.com/kode4food/sputter/assert"
+	e "github.com/kode4food/sputter/evaluator"
+)
+
+func TestClosure(t *testing.T) {
+	as := assert.New(t)
+
+	ns := a.GetNamespace(a.UserDomain)
+	ns.Delete("chain")
+	ns.Put("chain", s("hello"))
+
+	c1 := a.ChildContext(ns)
+	c1.Put("foo", s("foo_val"))
+	c1.Put("bar", s("bar_val"))
+	c1.Put("baz", s("baz_val"))
+
+	s1 := a.NewLocalSymbol("foo").Expression()
+	s2 := a.NewLocalSymbol("baz").Expression()
+	cl := e.NewClosure(a.NewVector(s1, s2))
+	v1 := cl.Eval(c1).(a.Vector)
+
+	r1, _ := v1.ElementAt(0)
+	r2, _ := v1.ElementAt(1)
+	as.String("foo_val", r1)
+	as.String("baz_val", r2)
+}
