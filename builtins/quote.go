860030d194eaa4860050f8ca16fe612bec40f619
Thomas Bradford <thom@kode4food.it>
2017-06-08 09:10:42 +0200

    forgot to add

---
+++ builtins/quote.go
@@ -0,0 +1,156 @@
+package builtins
+
+import (
+	"fmt"
+	"strings"
+	"sync/atomic"
+
+	a "github.com/kode4food/sputter/api"
+	d "github.com/kode4food/sputter/docstring"
+	e "github.com/kode4food/sputter/evaluator"
+)
+
+const genSymTemplate = "x-%s-gensym-%d"
+
+var (
+	sQuote  = a.NewQualifiedSymbol("quote", a.BuiltInDomain)
+	sList   = a.NewQualifiedSymbol("list", a.BuiltInDomain)
+	sVector = a.NewQualifiedSymbol("vector", a.BuiltInDomain)
+	sAssoc  = a.NewQualifiedSymbol("assoc", a.BuiltInDomain)
+)
+
+var genSymIncrement uint64
+
+type syntaxContext struct {
+	context a.Context
+	genSyms map[string]a.Value
+}
+
+func (sc *syntaxContext) quote(v a.Value) a.Value {
+	return sc.quoteValue(v)
+}
+
+func (sc *syntaxContext) quoteValue(v a.Value) a.Value {
+	if s, ok := v.(a.Sequence); ok {
+		return sc.quoteSequence(s)
+	}
+	if s, ok := v.(a.Symbol); ok {
+		return sc.quoteSymbol(s)
+	}
+	return v
+}
+
+func (sc *syntaxContext) quoteSymbol(s a.Symbol) a.Value {
+	if gs, ok := sc.genSym(s); ok {
+		return gs
+	}
+	return sc.qualifySymbol(s)
+}
+
+func (sc *syntaxContext) genSym(s a.Symbol) (a.Value, bool) {
+	if s.Domain() != a.LocalDomain {
+		return nil, false
+	}
+
+	n := string(s.Name())
+	if len(n) <= 1 || !strings.HasSuffix(n, "#") {
+		return nil, false
+	}
+
+	if r, ok := sc.genSyms[n]; ok {
+		return r, true
+	}
+
+	idx := atomic.AddUint64(&genSymIncrement, 1)
+	q := fmt.Sprintf(genSymTemplate, n[0:len(n)-1], idx)
+	r := a.NewList(sQuote, a.NewLocalSymbol(a.Name(q)))
+	sc.genSyms[n] = r
+	return r, true
+}
+
+func (sc *syntaxContext) qualifySymbol(s a.Symbol) a.Value {
+	if s.Domain() != a.LocalDomain {
+		return a.NewList(sQuote, s)
+	}
+	n := s.Name()
+	if c, ok := sc.context.Has(s.Name()); ok {
+		if c != sc.context {
+			ns := a.GetContextNamespace(c)
+			return a.NewList(sQuote, ns.Intern(n))
+		}
+	}
+	return s
+}
+
+func (sc *syntaxContext) quoteSequence(s a.Sequence) a.Value {
+	var sym a.Symbol
+	if l, ok := s.(a.List); ok && l.Count() > 0 {
+		sym = sList
+	} else if v, ok := s.(a.Vector); ok && v.Count() > 0 {
+		sym = sVector
+	} else if an, ok := s.(a.Associative); ok && an.Count() > 0 {
+		sym = sAssoc
+	} else {
+		return s
+	}
+	return a.NewList(sc.quoteElements(s)...).Prepend(sym)
+}
+
+func (sc *syntaxContext) quoteElements(s a.Sequence) []a.Value {
+	r := []a.Value{}
+	for i := s; i.IsSequence(); i = i.Rest() {
+		e := sc.quoteValue(i.First())
+		r = append(r, e)
+	}
+	return r
+}
+
+func quote(_ a.Context, args a.Sequence) a.Value {
+	a.AssertArity(args, 1)
+	return args.First()
+}
+
+func unquote(c a.Context, args a.Sequence) a.Value {
+	a.AssertArity(args, 1)
+	return e.Expand(c, args.First())
+}
+
+func syntaxquote(c a.Context, args a.Sequence) a.Value {
+	a.AssertArity(args, 1)
+	sc := &syntaxContext{
+		context: c,
+		genSyms: make(map[string]a.Value),
+	}
+	q := sc.quote(args.First())
+	r := e.Expand(c, q)
+	return r
+}
+
+func init() {
+	registerAnnotated(
+		a.NewMacro(quote).WithMetadata(a.Metadata{
+			a.MetaName: a.Name("quote"),
+			a.MetaDoc:  d.Get("quote"),
+		}),
+	)
+
+	registerAnnotated(
+		a.NewMacro(unquote).WithMetadata(a.Metadata{
+			a.MetaName:     a.Name("unquote"),
+			a.MetaSplicing: a.False,
+		}),
+	)
+
+	registerAnnotated(
+		a.NewMacro(unquote).WithMetadata(a.Metadata{
+			a.MetaName:     a.Name("unquote-splicing"),
+			a.MetaSplicing: a.True,
+		}),
+	)
+
+	registerAnnotated(
+		a.NewMacro(syntaxquote).WithMetadata(a.Metadata{
+			a.MetaName: a.Name("syntax-quote"),
+		}),
+	)
+}
---
+++ builtins/quote_test.go
@@ -0,0 +1,63 @@
+package builtins_test
+
+import (
+	"testing"
+
+	a "github.com/kode4food/sputter/api"
+	"github.com/kode4food/sputter/assert"
+	_ "github.com/kode4food/sputter/builtins"
+	e "github.com/kode4food/sputter/evaluator"
+)
+
+func TestQuote(t *testing.T) {
+	as := assert.New(t)
+
+	r1 := runCode("(quote (blah 2 3))").(a.List)
+	r2 := runCode("'(blah 2 3)").(a.List)
+
+	v1, ok := r1.ElementAt(0)
+	v2, _ := r2.ElementAt(0)
+	as.True(ok)
+	as.Equal(v1, v2)
+
+	v1, ok = r1.ElementAt(0)
+	as.True(ok)
+	if _, ok := v1.(a.Symbol); !ok {
+		as.Fail("first element is not a symbol")
+	}
+
+	v1, ok = r1.ElementAt(1)
+	v2, _ = r2.ElementAt(1)
+	as.True(ok)
+	as.Identical(v1, v2)
+
+	v1, ok = r1.ElementAt(1)
+	as.True(ok)
+	as.Number(2, v1)
+
+	v1, ok = r1.ElementAt(2)
+	v2, _ = r2.ElementAt(2)
+	as.True(ok)
+	as.Identical(v1, v2)
+
+	v1, ok = r1.ElementAt(2)
+	as.True(ok)
+	as.Number(3, v1)
+}
+
+func TestUnquote(t *testing.T) {
+	as := assert.New(t)
+
+	c := e.NewEvalContext()
+	c.Put("foo", a.NewFloat(456))
+	r1 := e.EvalStr(c, `'[123 ~foo]`)
+	as.String("[123 (sputter:unquote foo)]", r1)
+}
+
+func TestUnquoteMacro(t *testing.T) {
+	ns := a.GetNamespace(a.UserDomain)
+	ns.Delete("test")
+
+	runCode("(defmacro test [x & y] `(~x ~@y))")
+	testCode(t, "(test vector 1 2 3)", s("[1 2 3]"))
+}
