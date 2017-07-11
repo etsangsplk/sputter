bf66cb9fad785bfa4d4ab42e44a286d9c27ec520
Thomas Bradford <thom@kode4food.it>
2017-07-11 09:28:07 +0200

    bringing coverage back up

---
+++ api/lazy_test.go
@@ -0,0 +1,30 @@
+package api_test
+
+import (
+	"testing"
+
+	a "github.com/kode4food/sputter/api"
+	"github.com/kode4food/sputter/assert"
+)
+
+func TestLazySeq(t *testing.T) {
+	var inc a.Resolver
+	as := assert.New(t)
+
+	i := 0
+	inc = func() (a.Value, bool, a.Resolver) {
+		if i >= 10 {
+			return a.Nil, false, nil
+		}
+		i++
+		f := a.NewFloat(float64(i))
+		return f, true, inc
+	}
+
+	l := a.NewLazySequence(inc).Prepend(a.NewFloat(0))
+	as.True(l.IsSequence())
+	as.Number(0, l.First())
+	as.Number(1, l.Rest().First())
+	as.Number(2, l.Rest().Rest().First())
+	as.Contains(":type lazy-sequence", l.Str())
+}
